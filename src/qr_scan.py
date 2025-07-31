fc5f5aad7321a5e8b8991282a497028469fcdc3e
AntonSangho <sanghoemail@gmail.com>
2025-07-31 22:31:05 +0900

    바코드 정보 출력

--- src/qr_scan.py
+++ src/qr_scan.py
@@ -10,5 +10,9 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 plt.imshow(gray, cmap='gray')
 plt.show()
 
+decoded = pyzbar.decode(gray)
+
+print(decoded) 
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()

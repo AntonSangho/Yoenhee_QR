5bdc8413f903f7fadca8da272d7a2e822cc7ee42
AntonSangho <sanghoemail@gmail.com>
2025-07-31 23:04:05 +0900

    비디오로 동작하는 것 추가

--- src/qr_scan.py
+++ src/qr_scan.py
@@ -7,12 +7,20 @@ img = cv2.imread('../img/bh_bar.jpg')
 #plt.show()
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-plt.imshow(gray, cmap='gray')
-plt.show()
+#plt.imshow(gray, cmap='gray')
+#plt.show()
 
+# 디코딩
 decoded = pyzbar.decode(gray)
-
 print(decoded) 
+
+for d in decoded:
+    print(d.data.decode('utf-8'))
+    print(d.type)
+
+    cv2.rectangle(img, (d.rect[0], d.rect[1]), (d.rect[0] + d.rect[2], d.rect[1] + d.rect[3]), (0, 255, 0), 2)
+plt.imshow(img)
+plt.show()
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()

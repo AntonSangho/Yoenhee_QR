da5385168c0adf370c1f93948d1c1d2254441b6f
AntonSangho <sanghoemail@gmail.com>
2025-07-31 22:20:17 +0900

    바코드 보여지기

--- src/qr_scan.py
+++ src/qr_scan.py
@@ -1,2 +1,10 @@
 import cv2
 import matplotlib.pyplot as plt
+import pyzbar.pyzbar as pyzbar
+
+img = cv2.imread('../img/bh_bar.jpg')
+plt.imshow(img)
+plt.show()
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
